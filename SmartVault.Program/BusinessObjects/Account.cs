ab9f90c6454e55b3c193a31c64fcb52d88f53431
Rafael Rosa <rafaelxsantosx@hotmail.com>
2025-01-27 00:38:35 -0300

    Removes unused references.

--- SmartVault.Program/BusinessObjects/Account.cs
+++ SmartVault.Program/BusinessObjects/Account.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmartVault.Program.BusinessObjects
+﻿namespace SmartVault.Program.BusinessObjects
 {
     public partial class Account
     {
